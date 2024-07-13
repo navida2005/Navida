a4d8131111a9cafb97fd694e099bc4e9d5625ea3
Navida Gethmin Wimalaweera <navida@Navidas-MacBook-Air.local>
2024-07-13 13:57:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+let mybutton = document.getElementById("arrow");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function redirectToPage1() {
+    window.location.href = "https://github.com/navida2005/Time-Controlled-Relay-Module";
+}
+
+function redirectToPage2() {
+    window.location.href = "https://github.com/navida2005/Automated-Room-Simple";
+}
